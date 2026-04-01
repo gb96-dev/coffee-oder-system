e9be132af01e4c9b1c5b3e2b45f58a63f12f9fd3
김규범 <gb24404156@gmail.com>
2026-04-01 14:15:56 +0900

    feat: 커피 메뉴 목록 조회 API 구현 및 초기 데이터 추가

---
+++ src/main/java/com/cloud/coffee/controller/MenuController.java
@@ -0,0 +1,25 @@
+package com.cloud.coffee.controller;
+
+import com.cloud.coffee.domain.Menu;
+import com.cloud.coffee.service.MenuService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/menus")
+@RequiredArgsConstructor
+public class MenuController {
+
+    private final MenuService menuService;
+
+    @GetMapping
+    public ResponseEntity<List<Menu>> getAllMenus() {
+        List<Menu> menus = menuService.getAllMenus();
+        return ResponseEntity.ok(menus);
+    }
+}
